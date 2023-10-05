322cdc30676ad2c09f5a6baab53a449a08b163ae
YaroslavSokolov0372 <yaroslav.sokolov0372@gmail.com>
2023-10-05 10:33:53 +0200

    Initial Commit

---
+++ AlarmClockApp/AlarmClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockAppApp.swift
+//  AlarmClockApp
+//
+//  Created by Yaroslav Sokolov on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
